05e5a6c648765db27dd47daf0fa2107e1d7cf014
Laurent Demailly <ldemailly@gmail.com>
2025-11-21 15:30:24 -0800

    Version that can be embedded (sshdtui or demo) (#4)

---
+++ cli/NAME.go
@@ -0,0 +1,102 @@
+// NAME
+// DESCRIPTION
+
+package cli
+
+import (
+	"flag"
+	"os"
+	"runtime/pprof"
+
+	"fortio.org/cli"
+	"fortio.org/log"
+	"fortio.org/terminal/ansipixels"
+)
+
+type State struct {
+	AP         *ansipixels.AnsiPixels
+	MemProfile string
+}
+
+func Main() int {
+	truecolorDefault := ansipixels.DetectColorMode().TrueColor
+	fTrueColor := flag.Bool("truecolor", truecolorDefault,
+		"Use true color (24-bit RGB) instead of 8-bit ANSI colors (default is true if COLORTERM is set)")
+	fCpuprofile := flag.String("profile-cpu", "", "write cpu profile to `file`")
+	fMemprofile := flag.String("profile-mem", "", "write memory profile to `file`")
+	fFPS := flag.Float64("fps", 60, "Frames per second (ansipixels rendering)")
+	cli.Main()
+	if *fCpuprofile != "" {
+		f, err := os.Create(*fCpuprofile)
+		if err != nil {
+			return log.FErrf("can't open file for cpu profile: %v", err)
+		}
+		err = pprof.StartCPUProfile(f)
+		if err != nil {
+			return log.FErrf("can't start cpu profile: %v", err)
+		}
+		log.Infof("Writing cpu profile to %s", *fCpuprofile)
+		defer pprof.StopCPUProfile()
+	}
+	ap := ansipixels.NewAnsiPixels(*fFPS)
+	st := &State{
+		AP:         ap,
+		MemProfile: *fMemprofile,
+	}
+	ap.TrueColor = *fTrueColor
+	if err := ap.Open(); err != nil {
+		return 1 // error already logged
+	}
+	defer ap.Restore()
+	return st.Run()
+}
+
+func (st *State) Run() int {
+	ap := st.AP
+	ap.SyncBackgroundColor()
+	ap.OnResize = func() error {
+		ap.ClearScreen()
+		ap.StartSyncMode()
+		// Redraw/resize/do something here:
+		ap.WriteBoxed(ap.H/2-1, "Welcome to NAME!\n%dx%d\nQ to quit.", ap.W, ap.H)
+		// ...
+		ap.EndSyncMode()
+		return nil
+	}
+	_ = ap.OnResize()   // initial draw.
+	ap.AutoSync = false // for cursor to blink on splash screen. remove if not wanted.
+	err := ap.FPSTicks(st.Tick)
+	if st.MemProfile != "" {
+		f, errMP := os.Create(st.MemProfile)
+		if errMP != nil {
+			return log.FErrf("can't open file for mem profile: %v", errMP)
+		}
+		errMP = pprof.WriteHeapProfile(f)
+		if errMP != nil {
+			return log.FErrf("can't write mem profile: %v", errMP)
+		}
+		log.Infof("Wrote memory profile to %s", st.MemProfile)
+		_ = f.Close()
+	}
+	if err != nil {
+		log.Infof("Exiting on %v", err)
+		return 1
+	}
+	return 0
+}
+
+func (st *State) Tick() bool {
+	if len(st.AP.Data) == 0 {
+		return true
+	}
+	c := st.AP.Data[0]
+	switch c {
+	case 'q', 'Q', 3: // Ctrl-C
+		log.Infof("Exiting on %q", c)
+		return false
+	default:
+		log.Debugf("Input %q...", c)
+		// Do something
+	}
+	return true
+}
